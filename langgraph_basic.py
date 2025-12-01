6a4500b2b265934f6bbfe0cea50a27613289aa3c
MUHAMMAD NAGEEEB-UL ISLAM <kinematic7@users.noreply.github.com>
2025-11-30 20:01:44 -0500

    Add files via upload

--- langgraph_basic.py
+++ langgraph_basic.py
@@ -1,33 +1,33 @@
 from langgraph.graph import StateGraph, END
-from typing import TypedDict, List
+
+class State(dict):
+    step: {"state", ""}
 
 def start(state):
-    state["step"] = ["start"]
+    state["step"].append("start")
     return state
 
 def mid(state):
-    state["step"].append("mid")
+    state["step"].append("Middle")
     return state
 
 def end(state):
-    state["step"].append("end")
+    state["step"].append("End")
     return state
 
-
-class State(TypedDict):
-    step: List[str]
-
 graph = StateGraph(State)
+
 graph.add_node("start", start)
-graph.add_node("mid", mid)
+graph.add_node("middle", mid)
 graph.add_node("end", end)
 
 graph.set_entry_point("start")
-graph.add_edge("start", "mid")
-graph.add_edge("mid", "end")
+graph.add_edge("start", "middle")
+graph.add_edge("middle", "end")
 graph.add_edge("end", END)
 
 app = graph.compile()
 result = app.invoke({"step": []})
 
 print(result)
+
